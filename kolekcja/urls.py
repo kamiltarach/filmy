b10b0518caed314a206b7933e95e3e427d444f91
Kamil Tarach <113509888+kamiltarach@users.noreply.github.com>
2026-01-14 20:02:52 +0100

    Update - dodanie usuwania, zmiana interfejsu na "akordeonowy".

--- kolekcja/urls.py
+++ kolekcja/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.filmy_lista, name='filmy_lista'),
     path('nowy/', views.nowy_film, name='nowy_film'),
     path('edytuj/<int:id>/', views.edytuj_film, name='edytuj_film'),
+    # TEJ LINIJKI BRAKUJE i dlatego jest błąd:
+    path('usun/<int:id>/', views.usun_film, name='usun_film'),
 ]
--- kolekcja/views.py
+++ kolekcja/views.py
@@ -3,9 +3,14 @@ from .models import Film
 from .forms import FilmForm
 
 def filmy_lista(request):
-    filmy = Film.objects.all()
+    szukaj = request.GET.get('szukaj') 
+    
+    if szukaj:
+        filmy = Film.objects.filter(tytul__icontains=szukaj)
+    else:
+        filmy = Film.objects.all()
+        
     return render(request, 'kolekcja/filmy_lista.html', {'filmy': filmy})
-
 def nowy_film(request):
     if request.method == "POST":
         form = FilmForm(request.POST, request.FILES)
@@ -27,5 +32,14 @@ def edytuj_film(request, id):
             return redirect('filmy_lista')
     else:
         form = FilmForm(instance=film)
+
+def usun_film(request, id):
+    film = get_object_or_404(Film, pk=id)
+    
+    if request.method == "POST":
+        film.delete()
+        return redirect('filmy_lista')
+    
+    return render(request, 'kolekcja/potwierdz_usun.html', {'film': film})
     
     return render(request, 'kolekcja/filmy_form.html', {'form': form})
